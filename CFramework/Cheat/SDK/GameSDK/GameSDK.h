17ba9d6f782d4e54530835ee970e18680af12004
Neko64V <sawayump@gmail.com>
2025-02-01 04:20:22 +0900

    対象オフセットを追加

--- CFramework/Cheat/SDK/GameSDK/GameSDK.h
+++ CFramework/Cheat/SDK/GameSDK/GameSDK.h
@@ -15,6 +15,24 @@ namespace offset
 	constexpr auto dwGlobalVars		= cs2_dumper::offsets::client_dll::dwGlobalVars;
 	constexpr auto dwPlantedC4		= cs2_dumper::offsets::client_dll::dwPlantedC4;
 	
+	// Button
+	constexpr auto attack = 0x1875710;
+	constexpr auto attack2 = 0x18757A0;
+	constexpr auto back = 0x18759E0;
+	constexpr auto duck = 0x1875CB0;
+	constexpr auto forward = 0x1875950;
+	constexpr auto jump = 0x1875C20;
+	constexpr auto left = 0x1875A70;
+	constexpr auto lookatweapon = 0x1A9DF50;
+	constexpr auto reload = 0x1875680;
+	constexpr auto right = 0x1875B00;
+	constexpr auto showscores = 0x1A9DE30;
+	constexpr auto sprint = 0x18755F0;
+	constexpr auto turnleft = 0x1875830;
+	constexpr auto turnright = 0x18758C0;
+	constexpr auto use = 0x1875B90;
+	constexpr auto zoom = 0x1A9DEC0;
+
 	// CCSPlayerControlle
 	constexpr auto m_hPlayerPawn = cs2_dumper::schemas::client_dll::CCSPlayerController::m_hPlayerPawn;
 	constexpr auto m_sSanitizedPlayerName = cs2_dumper::schemas::client_dll::CCSPlayerController::m_sSanitizedPlayerName;
@@ -27,6 +45,10 @@ namespace offset
 	constexpr auto m_pCollision = cs2_dumper::schemas::client_dll::C_BaseEntity::m_pCollision;
 	constexpr auto m_pGameSceneNode = cs2_dumper::schemas::client_dll::C_BaseEntity::m_pGameSceneNode;
 	constexpr auto m_ArmorValue = cs2_dumper::schemas::client_dll::C_CSPlayerPawn::m_ArmorValue;
+
+	constexpr auto m_vecLastClipCameraPos = cs2_dumper::schemas::client_dll::C_CSPlayerPawnBase::m_vecLastClipCameraPos;
+	constexpr auto m_angEyeAngles = cs2_dumper::schemas::client_dll::C_CSPlayerPawnBase::m_angEyeAngles;
+	constexpr auto m_iIDEntIndex = cs2_dumper::schemas::client_dll::C_CSPlayerPawnBase::m_iIDEntIndex;
 	constexpr auto m_pClippingWeapon = cs2_dumper::schemas::client_dll::C_CSPlayerPawnBase::m_pClippingWeapon;
 	constexpr auto m_vOldOrigin = cs2_dumper::schemas::client_dll::C_BasePlayerPawn::m_vOldOrigin;
 	constexpr auto m_pCameraServices = cs2_dumper::schemas::client_dll::C_BasePlayerPawn::m_pCameraServices;
